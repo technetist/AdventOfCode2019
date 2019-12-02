f87df8411d95bc7701c6f5100707a685c6305949
Adrien Maranville <Adrien@Maranville.io>
2019-12-01 17:34:04 -0700

    first star

---
+++ Day1/app.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	filepath := "/Users/adrien/go/src/github.com/vehcklox/AdventOfCode2019/Day1/data-set.txt"
+	total := 0.00
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		value, err := strconv.ParseFloat(scanner.Text(), 10)
+		if err != nil {
+			panic(err)
+		}
+		tempValue := (math.Floor(value / 3)) -2
+		total += tempValue
+		fmt.Printf("%.1f\n", value)
+		fmt.Printf("%.1f\n", tempValue)
+		fmt.Printf("%.1f\n", total)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
